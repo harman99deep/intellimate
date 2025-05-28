6dbccdff7e33e274303e4d1d28c42dcb67c8a8cb
harman99deep <harman99deep@gmail.com>
2025-05-28 14:00:31 +0530

    updates

--- postgres/init/schema.sql
+++ postgres/init/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE historical_data (
     email TEXT,
     city TEXT,
     state TEXT);
+
+-- Load data from CSV
+COPY historical_data(customer_id, first_name, last_name, age, email, city, state)
+FROM 'C:\Users\HarmandeepSingh\data_monitoring_tool\postgres\imports\historical_data.csv'
+DELIMITER ','
+CSV HEADER;
+
+docker-entrypoint-initdb.d
